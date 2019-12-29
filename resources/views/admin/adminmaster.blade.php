1d13f3151a3ca99b6927aaaab6097f84cc4424bc
An <anbn247@gmail.com>
2019-12-29 14:44:50 +0700

    Coponent and router for admin

--- resources/views/admin/adminmaster.blade.php
+++ resources/views/admin/adminmaster.blade.php
@@ -6,11 +6,12 @@
     <title>AdminLTE 3 | Blank Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
 </head>
 <body class="hold-transition sidebar-mini">
 <!-- Site wrapper -->
-<div class="wrapper">
+<div class="wrapper" id="app">
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
 
@@ -47,31 +48,32 @@
                     <!-- Add icons to the links using the .nav-icon class
                          with font-awesome or any other icon font library -->
                     <li class="nav-item has-treeview">
-                        <a href="#" class="nav-link">
+                        <router-link to="/home" class="nav-link">
                             <i class="nav-icon fa fa-dashboard"></i>
                             <p>
                                 Dashboard
                                 <i class="right fa fa-angle-left"></i>
                             </p>
-                        </a>
+                        </router-link>
 
                     </li>
                     <li class="nav-item">
-                        <a href="../widgets.html" class="nav-link">
+                        <router-link to="/category-list" class="nav-link">
                             <i class="nav-icon fa fa-th"></i>
                             <p>
                                 Category
 
                             </p>
-                        </a>
+                        </router-link>
                     </li>
                     <li class="nav-item">
-                        <a href="../widgets.html" class="nav-link">
+                        <router-link to="/post"  class="nav-link">
                             <i class="nav-icon fa fa-th"></i>
                             <p>
-                                Post                                <span class="right badge badge-danger">New</span>
+                                Post
+                                <span class="right badge badge-danger">New</span>
                             </p>
-                        </a>
+                        </router-link>
                     </li>
 
                 </ul>
@@ -83,36 +85,8 @@
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-
-
         <!-- Main content -->
-        <section class="content">
-
-            <!-- Default box -->
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Title</h3>
-
-                    <div class="card-tools">
-                        <button type="button" class="btn btn-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-                            <i class="fa fa-minus"></i></button>
-                        <button type="button" class="btn btn-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-                            <i class="fa fa-times"></i></button>
-                    </div>
-                </div>
-                <div class="card-body">
-                    Start creating your amazing application!
-                </div>
-                <!-- /.card-body -->
-                <div class="card-footer">
-                    Footer
-                </div>
-                <!-- /.card-footer-->
-            </div>
-            <!-- /.card -->
-
-        </section>
+        <admin-main></admin-main>
         <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('post', 'PostController@index');
+
+Route::get('/{anypath}','HomeController@index')->where('path','.*');
